b48eb37e2102556f9b34d2275d2a469d6c2a48cc
Koshal Sain <koshalsain@Koshals-MacBook-Air.local>
2023-08-01 14:30:40 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, {
         dbName: "UserApi",
-    }).then(() => {
-        console.log("Successfully Connected");
+    }).then((c) => {
+        console.log(`Successfully Connected with ${c.connection.host}`);
     }).catch((err) => {
         console.log(err);
     })
